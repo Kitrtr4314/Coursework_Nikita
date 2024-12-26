5cc6c1e7285707861e79f5cc0a037aea14f571a8
Nikita Iakovlev <nikitayakovlev548@gmail.com>
2024-12-26 18:41:01 +0300

    add eventhandles

--- StarSystemApp/MainForm.cs
+++ StarSystemApp/MainForm.cs
@@ -1,33 +1,73 @@
 using System;
+using System.Linq;
 using System.Windows.Forms;
+using StarSystemLibrary;
 
 namespace StarSystemApp
 {
     public partial class MainForm : Form
     {
+        private readonly StarSystem starSystem = new StarSystem();
+
         public MainForm()
         {
             InitializeComponent();
+            UpdateListBox();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            throw new System.NotImplementedException();
-        }
-
+        // Обработчик события добавления объекта
         private void button1_Click(object sender, EventArgs e)
         {
-            throw new System.NotImplementedException();
+            var addForm = new AddObjectForm();
+            if (addForm.ShowDialog() == DialogResult.OK)
+            {
+                var newObject = addForm.CreatedObject;
+                if (newObject != null)
+                {
+                    starSystem.AddObject(newObject);
+                    UpdateListBox();
+                }
+            }
         }
 
+        // Обработчик события удаления объекта
         private void DeleteObject_Click(object sender, EventArgs e)
         {
-            throw new System.NotImplementedException();
+            if (ListBoxObject.SelectedItem is SpaceObject selectedObject)
+            {
+                starSystem.RemoveObject(selectedObject);
+                UpdateListBox();
+            }
+            else
+            {
+                MessageBox.Show("Выберите объект для удаления.", "Ошибка");
+            }
+        }
+
+        // Обработчик события отображения информации
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ListBoxObject.SelectedItem is SpaceObject selectedObject)
+            {
+                ObjectLabel.Text = selectedObject.GetInfo();
+            }
+        }
+
+        // Обработчик события сортировки
+        private void Sort_Click(object sender, EventArgs e)
+        {
+            starSystem.SortByMass(); // Допустим, сортируем по массе
+            UpdateListBox();
+        }
+
+        // Метод обновления списка в ListBox
+        private void UpdateListBox()
+        {
+            ListBoxObject.Items.Clear();
+            foreach (var obj in starSystem.GetObjects())
+            {
+                ListBoxObject.Items.Add(obj);
+            }
         }
     }
 }
