a97314722dd1a36e7610a4a0718948535f6a9f21
NarkisYakhin <100925800+NarkisYakhin@users.noreply.github.com>
2024-12-27 15:34:24 +0300

    add EditObjectForm.cs

---
+++ StarSystemApp/EditObjectForm.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace StarSystemApp
+{
+    public partial class EditObjectForm : Form
+    {
+        public EditObjectForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
