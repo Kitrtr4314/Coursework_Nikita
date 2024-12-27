9d61ab99ef0738c31d16438f35da7652a6bdbb03
Nikita Iakovlev <nikitayakovlev548@gmail.com>
2024-12-27 15:39:18 +0300

    edit object

--- StarSystemApp/EditObjectForm.Designer.cs
+++ StarSystemApp/EditObjectForm.Designer.cs
@@ -1,42 +1,49 @@
-using System.ComponentModel;
-
 namespace StarSystemApp
 {
     partial class EditObjectForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private IContainer components = null;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtMass;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtDiameter;
+        private System.Windows.Forms.TextBox txtPlanetName;
+        private System.Windows.Forms.NumericUpDown nudLuminosity;
+        private System.Windows.Forms.NumericUpDown nudMoonsCount;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.RadioButton rbStar;
+        private System.Windows.Forms.RadioButton rbPlanet;
+        private System.Windows.Forms.RadioButton rbMoon;
+        private System.Windows.Forms.GroupBox groupBox1;
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
             {
                 components.Dispose();
             }
-
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "EditObjectForm";
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbStar = new System.Windows.Forms.RadioButton();
+            this.rbPlanet = new System.Windows.Forms.RadioButton();
+            this.rbMoon = new System.Windows.Forms.RadioButton();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtMass = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtDiameter = new System.Windows.Forms.TextBox();
+            this.txtPlanetName = new System.Windows.Forms.TextBox();
+            this.nudLuminosity = new System.Windows.Forms.NumericUpDown();
+            this.nudMoonsCount = new System.Windows.Forms.NumericUpDown();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            // Добавьте все элементы и их обработчики, как в `AddObjectForm`
         }
-
-        #endregion
     }
 }
--- StarSystemApp/EditObjectForm.cs
+++ StarSystemApp/EditObjectForm.cs
@@ -1,12 +1,155 @@
+using System;
 using System.Windows.Forms;
+using StarSystemLibrary;
 
 namespace StarSystemApp
 {
     public partial class EditObjectForm : Form
     {
-        public EditObjectForm()
+        public SpaceObject EditedObject { get; private set; }
+
+        public EditObjectForm(SpaceObject existingObject)
         {
             InitializeComponent();
+            EditedObject = existingObject;
+            FillFormFields();
+            ToggleInputs();
+        }
+
+        private void FillFormFields()
+        {
+            // Заполнение полей данными существующего объекта
+            txtName.Text = EditedObject.Name;
+            txtMass.Text = EditedObject.Mass.ToString();
+            txtDiameter.Text = EditedObject.EquatorialDiameter.ToString();
+            txtAge.Text = EditedObject.Age.ToString();
+
+            // В зависимости от типа объекта, включаем нужные поля
+            if (EditedObject is Star star)
+            {
+                rbStar.Checked = true;
+                nudLuminosity.Value = (decimal)star.Luminosity;
+            }
+            else if (EditedObject is Planet planet)
+            {
+                rbPlanet.Checked = true;
+                nudMoonsCount.Value = planet.MoonsCount;
+            }
+            else if (EditedObject is Moon moon)
+            {
+                rbMoon.Checked = true;
+                txtPlanetName.Text = moon.PlanetName;
+            }
+        }
+
+        private void ToggleInputs()
+        {
+            // Блокируем или разблокируем поля в зависимости от типа объекта
+            if (rbStar.Checked)
+            {
+                nudLuminosity.Enabled = true;
+                nudMoonsCount.Enabled = false;
+                txtPlanetName.Enabled = false;
+            }
+            else if (rbPlanet.Checked)
+            {
+                nudLuminosity.Enabled = false;
+                nudMoonsCount.Enabled = true;
+                txtPlanetName.Enabled = false;
+            }
+            else if (rbMoon.Checked)
+            {
+                nudLuminosity.Enabled = false;
+                nudMoonsCount.Enabled = false;
+                txtPlanetName.Enabled = true;
+            }
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Проверка на валидность массы, диаметра и возраста
+                if (!float.TryParse(txtMass.Text, out var mass) || mass <= 0)
+                {
+                    MessageBox.Show("Масса должна быть числом больше 0.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (!float.TryParse(txtDiameter.Text, out var diameter) || diameter <= 0)
+                {
+                    MessageBox.Show("Диаметр должен быть числом больше 0.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (!int.TryParse(txtAge.Text, out var age) || age <= 0)
+                {
+                    MessageBox.Show("Возраст должен быть числом больше 0.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                string name = txtName.Text;
+
+                if (rbStar.Checked)
+                {
+                    var luminosity = (float)nudLuminosity.Value;
+                    EditedObject = new Star
+                    {
+                        Mass = mass,
+                        EquatorialDiameter = diameter,
+                        Age = age,
+                        Luminosity = luminosity,
+                        Name = name
+                    };
+                }
+                else if (rbPlanet.Checked)
+                {
+                    var moonCount = (int)nudMoonsCount.Value;
+                    EditedObject = new Planet
+                    {
+                        Mass = mass,
+                        EquatorialDiameter = diameter,
+                        Age = age,
+                        MoonsCount = moonCount,
+                        Name = name
+                    };
+                }
+                else if (rbMoon.Checked)
+                {
+                    var planetName = txtPlanetName.Text;
+                    EditedObject = new Moon
+                    {
+                        Mass = mass,
+                        EquatorialDiameter = diameter,
+                        Age = age,
+                        PlanetName = planetName,
+                        Name = name
+                    };
+                }
+                else
+                {
+                    MessageBox.Show("Выберите тип объекта.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Пожалуйста, введите корректные значения во все поля.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void RadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            ToggleInputs();
         }
     }
 }
