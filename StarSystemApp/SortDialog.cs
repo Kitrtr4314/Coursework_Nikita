117ee869cae89c6292af6fe808ed5bd814e91c34
NarkisYakhin <100925800+NarkisYakhin@users.noreply.github.com>
2024-12-27 12:21:21 +0300

    refact

---
+++ StarSystemApp/SortDialog.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace StarSystemApp
+{
+    public partial class SortDialog : Form
+    {
+        public SortDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
